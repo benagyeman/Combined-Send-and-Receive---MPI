4b5852a0168e12f1d42afba0bfada32e59906d6a
Bernard Agyeman <40953282+kbagyeman@users.noreply.github.com>
2022-11-11 10:42:05 -0700

    Add files via upload

---
+++ mpi_chain.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<mpi.h>
+using namespace std;
+using namespace MPI;
+
+
+//Use a for-loop to implement message passing between processes in a chain
+// Assuming there are n processes, the proc 0 ---> proc 1 ---> proc 2 ....... proc n-1
+int main(int argc, char ** argv)
+{
+    Init();
+    int num_procs = COMM_WORLD.Get_size();
+    for (int i=0; i<num_procs-1; i++)
+    {
+        if(COMM_WORLD.Get_rank()==i)// Send the message
+        {   
+            int rank_send = i;
+            int tag = 20;
+            int recv_rank = (i+1);
+            COMM_WORLD.Send(&rank_send, 1, INT, recv_rank, tag);
+        }
+
+        if(COMM_WORLD.Get_rank()==(i+1)) // Receive the message
+        {  
+            int rank_recv;
+            int tag_new = 20;
+            int send_rank = (i + 1);
+            COMM_WORLD.Recv(&rank_recv, 1, INT, i , tag_new);
+            cout<<"Process "<< send_rank<< " received from "<< rank_recv<<"\n";
+        }
+    }
+    Finalize();
+    return 0;
+}
